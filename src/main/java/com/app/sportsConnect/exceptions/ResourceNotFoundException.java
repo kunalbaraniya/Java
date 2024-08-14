de6a067ccbd4ee41d3b573a3df585b8bdcdc3c43
ChiragV <vermachirag1108@gmail.com>
2024-08-14 19:24:14 +0530

    [x] removed unused files

--- src/main/java/com/app/sportsConnect/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.sportsConnect.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
